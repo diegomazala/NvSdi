f00e615fde31404a33be8d09291b5f0fb656345c
Diego <diegomazala@gmail.com>
2016-10-24 13:18:16 -0200

    Renamed Dvp script

--- unity/Assets/GLNvSdi/Scripts/GLNvDvpUI.cs
+++ unity/Assets/GLNvSdi/Scripts/GLNvDvpUI.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [AddComponentMenu("Diem/Video IO SDI - GLNvSdiIO")]
 public class GLNvDvpUI : MonoBehaviour
 {
-    public DvpIO dvp;
+    public GLNvDvp dvp;
 
     public UnityEngine.UI.Text videoFormat;
     public UnityEngine.UI.Text syncSource;
@@ -19,7 +19,7 @@ public class GLNvDvpUI : MonoBehaviour
 
     void Start()
     {
-        dvp = FindObjectOfType<DvpIO>();
+        dvp = FindObjectOfType<GLNvDvp>();
         if (dvp == null)
         {
             Debug.LogError("Could not find dvp object. Disabling dvp UI");
@@ -32,9 +32,9 @@ public class GLNvDvpUI : MonoBehaviour
 
     void Update()
     {
-        droppedFramesIn.text = DvpIO.Plugin.DvpInputDroppedFrames().ToString();
-        droppedFramesOut.text = DvpIO.Plugin.DvpDroppedFrames().ToString();
-        captureTime.text = DvpIO.Plugin.DvpInputCaptureElapsedTime().ToString("0.0000");
+        droppedFramesIn.text = GLNvDvp.Plugin.DvpInputDroppedFrames().ToString();
+        droppedFramesOut.text = GLNvDvp.Plugin.DvpDroppedFrames().ToString();
+        captureTime.text = GLNvDvp.Plugin.DvpInputCaptureElapsedTime().ToString("0.0000");
     }
 
 
@@ -50,16 +50,15 @@ public class GLNvDvpUI : MonoBehaviour
 
     public void OnReloadUI()
     {
-        Debug.Log("reload");
         videoFormat.text = dvp.options.videoFormat.ToString();
         syncSource.text = dvp.options.syncSource.ToString();
         ringBufferIn.text = dvp.options.inputRingBufferSize.ToString();
         ringBufferOut.text = dvp.options.outputRingBufferSize.ToString();
         hSync.text = dvp.options.outputHorizontalDelay.ToString();
         vSync.text = dvp.options.outputVerticalDelay.ToString();
-        droppedFramesIn.text = DvpIO.Plugin.DvpInputDroppedFrames().ToString();
-        droppedFramesOut.text = DvpIO.Plugin.DvpDroppedFrames().ToString();
-        captureTime.text = DvpIO.Plugin.DvpInputCaptureElapsedTime().ToString("0.0000");
+        droppedFramesIn.text = GLNvDvp.Plugin.DvpInputDroppedFrames().ToString();
+        droppedFramesOut.text = GLNvDvp.Plugin.DvpDroppedFrames().ToString();
+        captureTime.text = GLNvDvp.Plugin.DvpInputCaptureElapsedTime().ToString("0.0000");
     }
 
 }
