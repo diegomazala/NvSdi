64abecf4e1e0d82000696fde5a1ac33f8d7953d8
Diego <diegomazala@gmail.com>
2017-04-03 17:25:26 -0300

    Start time to log file

--- src/GLNvSdi/GLNvSdi.cpp
+++ src/GLNvSdi/GLNvSdi.cpp
@@ -172,6 +172,13 @@ extern "C"
 	GLNVSDI_API void SdiSetupLogFile()
 	{
 		attr::gSdiLogger.ToFile("GLNvSdi");
+		time_t t = time(0);   // get time now
+		struct tm * now = localtime(&t);
+		std::cout << (now->tm_year + 1900) << '-'
+			<< (now->tm_mon + 1) << '-'
+			<< now->tm_mday << " - "
+			<< now->tm_hour << ':' << now->tm_min << ':' << now->tm_sec
+			<< std::endl;
 	}
 
 
